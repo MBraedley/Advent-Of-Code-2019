daf820427501a002dc51f577676385a600d837de
Michael Braedley <mike.braedley@gmail.com>
2019-12-08 20:41:17 -0400

    Day 7 start

--- Day07/Day07/Day07.cpp
+++ Day07/Day07/Day07.cpp
@@ -1,20 +1,28 @@
 // Day07.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include "IntcodeComputer.h"
+
 #include <iostream>
+#include <fstream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::vector<int> input;
+	std::vector<int> program;
+
+	int value;
+	char split;
+
+	std::ifstream fileIn("input.txt");
+
+	while (fileIn >> value >> split)
+	{
+		input.push_back(value);
+	}
+	fileIn >> value;
+	input.push_back(value);
+
+	program.assign(input.begin(), input.end());
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
