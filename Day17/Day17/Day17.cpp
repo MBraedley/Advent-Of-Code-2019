3ca57d9ccfb12ab1915d7996c1b7f04c95a3429f
MBraedley <mike.braedley@gmail.com>
2019-12-23 14:23:06 -0400

    Day 17 solved with some help from the solution megathread

--- Day17/Day17/Day17.cpp
+++ Day17/Day17/Day17.cpp
@@ -4,12 +4,14 @@
 #include "IntcodeComputer.h"
 
 #include <iostream>
+#include <deque>
 
 int main()
 {
 	auto input = IntcodeComputer::RaedProgram("input.txt");
+	auto program = input;
 
-	IntcodeComputer computer(input);
+	IntcodeComputer computer(program);
 
 	std::string video;
 	computer.SetOutputCallback([&](std::int64_t output)
@@ -55,4 +57,42 @@ int main()
 	}
 
 	std::cout << alignmentParam << std::endl;
+
+	program = input;
+	program[0] = 2;
+
+	std::string mainRoutine = "A,B,A,C,B,A,B,C,C,B\n";
+	std::string moveA = "L,12,L,12,R,4\n";
+	std::string moveB = "R,10,R,6,R,4,R,4\n";
+	std::string moveC = "R,6,L,12,L,12\n";
+
+	std::deque<char> movements;
+	movements.insert(movements.end(), mainRoutine.begin(), mainRoutine.end());
+	movements.insert(movements.end(), moveA.begin(), moveA.end());
+	movements.insert(movements.end(), moveB.begin(), moveB.end());
+	movements.insert(movements.end(), moveC.begin(), moveC.end());
+	movements.push_back('n');
+	movements.push_back('\n');
+
+	IntcodeComputer part2(program);
+	
+	part2.SetInputCallback([&]()->std::int64_t
+		{
+			std::int64_t input = static_cast<std::int64_t>(movements.front());
+			movements.pop_front();
+			return input;
+		});
+
+	part2.SetOutputCallback([&](std::int64_t output)
+		{
+			if (output < 256)
+			{
+				video.push_back(static_cast<char>(output));
+				std::cout << static_cast<char>(output);
+			}
+			else
+				std::cout << "Dust: " << output << std::endl;
+		});
+
+	part2.RunProgram();
 }
